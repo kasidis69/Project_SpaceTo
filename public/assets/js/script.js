a049e59494809fda44eca943f77238dd97c266ad
unknown <apichaya.peach@mail.kmutt.ac.th>
2021-04-30 22:54:57 +0700

    แก้นิดหน่อย เหลือequitment

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,5 +10,15 @@
     document.getElementById(popoverID).classList.toggle("hidden");
   }
 
+/*หน้า add(กรอกข้อมูลสถานที่) 
+function show_location_other()
+{
+
+    if (document.getElementById('location').value == "Other" ){
+	document.getElementById('location_other').style.display = 'block';}
+    else {document.getElementById('location_other').style.display = 'none';}
+
+}
+*/
 
 
