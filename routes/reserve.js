26b7f4a81933e65f5278c9c94ad6e9300402d960
wijakgrt <wijak1203@gmail.com>
2021-11-29 12:33:49 +0700

    final

--- routes/reserve.js
+++ routes/reserve.js
@@ -102,7 +102,7 @@ module.exports = {
           let c  = a+""+b  
 
           var arr = c.split(" ");
-          datetime = arr[2]+"  "+arr[1]+"  "+arr[4]+" - "+arr[10];
+          datetime = arr[2]+"  "+arr[1]+"  "+arr[4]+" - "+arr[12];
           
           array.push(datetime)
        })
@@ -246,7 +246,7 @@ module.exports = {
           let c  = a+""+b  
 
           var arr = c.split(" ");
-          datetime = arr[2]+"  "+arr[1]+"  "+arr[4]+" - "+arr[10];
+          datetime = arr[2]+"  "+arr[1]+"  "+arr[4]+" - "+arr[11];
           array.push(datetime)
           
        })
